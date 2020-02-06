2977b8ae70d1b153609ad26689f271caf5fa9716
Rand al'Thor <alexanderhogle@gmail.com>
2020-02-06 01:37:37 -0600

    adds working get requests for html routes

--- server.js
+++ server.js
@@ -10,6 +10,25 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//get request for html
+// app.get("/notes", function(req, res) {
+//     res.sendFile(path.join(path.join(__dirname, "/public"), "notes.html"));
+// });
+
+// app.get("*", function(req, res) {
+//     res.sendFile(path.join(path.join(__dirname, "/public"), "index.html"));
+// });
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+
+
 //Starts server
 app.listen(PORT, function() {
     console.log("App listening on http://localhost:" + PORT);
